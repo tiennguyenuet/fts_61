224fb3fbbea4115ecefa43491401395c9fd9b769
hieunk58 <khachieunk@gmail.com>
2016-07-14 08:35:25 +0700

    admin create subject

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -1,6 +1,27 @@
 class Admin::SubjectsController < ApplicationController
+  load_and_authorize_resource
+
   def index
     @subjects = Subject.order(created_at: :desc)
       .page(params[:page]).per Settings.per_page
+  end
+
+  def new
+  end
+
+  def create
+    if @subject.save
+      flash[:success] = t "controlllers.admin.subject_controller.create_success"
+      redirect_to admin_subjects_path
+    else
+      flash[:danger] =  t "controlllers.admin.subject_controller.create_error"
+      render :new
+    end
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit :name, :total_question,
+      :duration, :description
   end
 end
--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,4 +1,12 @@
 class Subject < ActiveRecord::Base
   has_many :examinations, dependent: :destroy
   has_many :questions, dependent: :destroy
+
+  validates :name, presence: true, length: {maximum: 50},
+    uniqueness: {case_sensitive: false}
+  validates :description, presence: true, length: {maximum: 150}
+  validates :total_question, presence: true,
+    numericality: {only_integer: true, greater_than: 0}
+  validates :duration, presence: true,
+    numericality: {only_integer: true, greater_than: 0}
 end
