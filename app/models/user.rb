47807326910e023b2c4599684fbe0bcb50041f7c
Tien Nguyen <tiennguyenuet@gmail.com>
2016-07-15 10:27:46 +0700

    signup signin signout

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :examinations, dependent: :destroy
   has_many :activities, dependent: :destroy
   has_many :questions, dependent: :destroy
