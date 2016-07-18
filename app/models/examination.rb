665931a33bb9d78bf28095bee38725b55c33798a
hieunk58 <khachieunk@gmail.com>
2016-07-18 12:49:18 +0700

    user create exam

--- app/models/examination.rb
+++ app/models/examination.rb
@@ -2,4 +2,23 @@ class Examination < ActiveRecord::Base
   belongs_to :user
   belongs_to :subject
   has_many :results, dependent: :destroy
+  has_many :questions, through: :results
+
+  accepts_nested_attributes_for :results,
+    reject_if: lambda{|att| att[:question_id].blank?}, allow_destroy: true
+
+  enum status: [:start, :testing, :unchecked, :checked]
+
+  before_create :create_questions
+
+  private
+  def create_questions
+    total_question = subject.questions.size
+    questions = subject.questions.order("RANDOM()")
+    if Settings.number_question1 == total_question
+      duration = Settings.duration1
+    else
+      duration = Settings.duration2
+    end
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,22 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create name: "admin", email: "admin@example.com", role: 1,
   password: "12345678", password_confirmation: "12345678"
+
+20.times do |u|
+  User.create! name: "User" + (u+1).to_s,
+  email: "user#{u+1}@example.com",
+  password: "12345678"
+end
+
+20.times do |u|
+  Subject.create! name: "Git" + (u+1).to_s,
+  description: "Git for beginner",
+  total_question: 20,
+  duration: 30
+end
+
+20.times do |u|
+  Examination.create! status: 0,
+  user_id: 1,
+  subject_id: (u+1)
+end
