f9d656eb5fbd2e2b8499eee1ec3fec34410f4ed7
hieunk58 <khachieunk@gmail.com>
2016-07-21 14:34:14 +0700

    user authenticate via facebook

--- Gemfile
+++ Gemfile
@@ -20,7 +20,7 @@ gem "turbolinks"
 gem "jbuilder", "~> 2.0"
 # bundle exec rake doc:rails generates the API under doc/api.
 gem "sdoc", "~> 0.4.0", group: :doc
-gem "bootstrap-sass"
+gem "bootstrap-sass", "3.2.0.0"
 
 gem "devise", "~>3.4.1"
 # Use ActiveModel has_secure_password
@@ -36,6 +36,7 @@ gem "kaminari"
 gem "public_activity"
 gem "omniauth"
 gem "omniauth-google-oauth2"
+gem "omniauth-facebook"
 gem "figaro"
 gem "ransack"
 
--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,6 +6,16 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
+    end
+  end
+  def facebook
+    @user = User.from_omniauth request.env["omniauth.auth"]
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message :notice, :success,
+        kind: "Facebook" if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
