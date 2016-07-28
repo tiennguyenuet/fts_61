ff49b88477f31a8818882765dfb997782f47dbfe
hieunk58 <khachieunk@gmail.com>
2016-07-28 10:05:10 +0700

    system send result to user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,10 @@ class UserMailer < ApplicationMailer
     @subject = subject
     mail to: @user.email, subject: "#{subject.name}"
   end
+
+  def send_exam_result exam
+    @exam = exam
+    @user = exam.user
+    mail to: @user.email, subject: t("views.user_mailer.send_result_title")
+  end
 end
